a2edd25136df0fce5c9c526a4c6c5f08d9b52068
tyron12233 <lucena.tyronscott@gmail.com>
2025-06-24 01:31:00 +0800

    chore: update build

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  distDir: "dist",
-  basePath: "/animochat-turn-server",
-  assetPrefix: "/animochat-turn-server/",
-  images: {
-    unoptimized: true,
-  }
 };
 
 export default nextConfig;
